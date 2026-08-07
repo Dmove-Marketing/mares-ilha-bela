075ebf91181f2fb36e5aa74d7b88beb8cfaa00d1
Dmove Marketing <dev@dmove.com.br>
2026-08-07 11:07:58 -0300

    chore(deploy): automatically fix remote permissions after scp

--- deploy.mjs
+++ deploy.mjs
@@ -14,3 +14,7 @@ if (!server) {
 const dest = `${user}@${server}:${remote_path}`;
 console.log(`Enviando dist/ → ${dest}`);
 execSync(`scp -r dist/* ${dest}`, { stdio: "inherit" });
+
+console.log(`Ajustando permissões no servidor remoto...`);
+execSync(`ssh ${user}@${server} "find ${remote_path} -type d -exec chmod 755 {} \\; && find ${remote_path} -type f -exec chmod 644 {} \\;"`, { stdio: "inherit" });
+
